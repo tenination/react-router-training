62b5ab4e9968135bcf197706a2092ae74570479e
Tim Ninan <tninan@C02TH0ZXGTFM.local>
2019-01-20 21:00:58 -0600

    basic react router setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Link, Route } from 'react-router-dom';
+
+const Home = () => (
+  <div>
+    <h2> Home </h2>
+  </div>
+);
+
+const Airport = () => (
+  <div>
+    <h2> Airport </h2>
+    <ul>
+      <li>Jomo Kenyatta</li>
+      <li>Tambo</li>
+      <li>Murtala Mohammed</li>
+    </ul>
+  </div>
+);
+
+const City = () => (
+  <div>
+    <h2> City </h2>
+    <ul>
+      <li>San Francisco</li>
+      <li>Istanbul</li>
+      <li>Tokyo</li>
+    </ul>
+  </div>
+);
+
+const Courses = ({ match }) => (
+  <div>
+    <h2> Courses </h2>
+    {console.log('match:', match)}
+    <ul>
+      <li>
+        <Link to={`${match.url}/technology`}>Technology</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/business`}>Business</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/economics`}>Economics</Link>
+      </li>
+    </ul>
+
+    <Route
+      exact
+      path={`${match.path}/:course`}
+      render={({ match }) => <div> This is {match.params.course}</div>}
+    />
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/airports">Airports</Link>
+          </li>
+          <li>
+            <Link to="/cities">Cities</Link>
+          </li>
+          <li>
+            <Link to="/courses">Courses</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/airports" component={Airport} />
+        <Route path="/cities" component={City} />
+        <Route path="/courses" component={Courses} />
+
+        <Route
+          path="/products"
+          render={() => <div> This is the products route </div>}
+        />
       </div>
     );
   }
